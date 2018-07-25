1d913a1eb9b3e995663ce13322ba09e1523d9406
Tim Jacomb <t.jacomb@kainos.com>
2018-07-25 10:20:58 +0100

    Rename moj to cnp (#119)

--- roles/jenkins/files/orchestrator.groovy
+++ roles/jenkins/files/orchestrator.groovy
@@ -3,27 +3,27 @@ node {
 
   environmnent = 'prod'
   //build core infra
-  build job: 'contino/moj-core-infrastructure/parametrised',
+  build job: 'hmcts/cnp-core-infrastructure/parametrised',
       parameters: [string(name: 'PRODUCT_NAME', value: String.valueOf('core-infra')),
                    string(name: 'ENV_NAME', value: environmnent)]
 
   //build core compute
-  build job: 'contino/moj-core-compute/parametrised',
+  build job: 'hmcts/cnp-core-compute/parametrised',
       parameters: [string(name: 'PRODUCT_NAME', value: String.valueOf('core-compute')),
                    string(name: 'ENV_NAME', value: environmnent)]
 
   //deploy rhubarb
-  build job: 'contino/moj-rhubarb-infrastructure/parametrised',
+  build job: 'hmcts/cnp-rhubarb-infrastructure/parametrised',
       parameters: [string(name: 'PRODUCT_NAME', value: String.valueOf('rhubarb')),
                    string(name: 'ENV_NAME', value: environmnent)]
 
   parallel {
 
-    build job: 'contino/moj-rhubarb-frontend/parametrised',
+    build job: 'hmcts/cnp-rhubarb-frontend/parametrised',
         parameters: [string(name: 'PRODUCT_NAME', value: String.valueOf('rhubarb')),
                      string(name: 'ENV_NAME', value: environmnent)]
 
-    build job: 'contino/moj-rhubarb-recipes-service/parametrised',
+    build job: 'hmcts/cnp-rhubarb-recipes-service/parametrised',
         parameters: [string(name: 'PRODUCT_NAME', value: String.valueOf('rhubarb')),
                      string(name: 'ENV_NAME', value: environmnent)]
   }
