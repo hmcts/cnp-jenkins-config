b377cbea4e9a733dcc0f332e1cad078fba92a7e1
Tim Jacomb <tim.jacomb@hmcts.net>
2019-01-31 10:07:58 +0000

    Updates to work for everything

--- jobdsl/organisations.groovy
+++ jobdsl/organisations.groovy
@@ -2,7 +2,7 @@ String url = jenkins.model.JenkinsLocationConfiguration.get().getUrl()
 println "Running on ${url}"
 
 private boolean isSandbox() {
-    jenkins.model.JenkinsLocationConfiguration.get().getUrl().contains("sandbox")
+    jenkins.model.JenkinsLocationConfiguration.get().getUrl().contains("localhost")
 }
 
 List<Map> orgs = [
@@ -12,24 +12,67 @@ List<Map> orgs = [
 ]
 orgs.each { Map org ->
     githubOrg(org).call()
+    org << [nightly: true]
+    if (!org.nightlyDisabled) {
+        githubOrg(org).call()
+    }
 }
+
+if (isSandbox()) {
+    Map pipelineTestOrg = [
+            name                      : 'pipeline_test',
+            displayName               : 'Pipeline Test',
+            regex                     : 'cnp-rhubarb-.*|cnp-jenkins-library',
+            branchesToInclude         : 'master',
+            jenkinsfilePath           : 'Jenkinsfile_pipeline_test',
+            suppressDefaultJenkinsfile: true
+    ]
+    githubOrg(pipelineTestOrg).call()
+}
+
 Closure githubOrg(Map args = [:]) {
     def config = [
-            displayName     : args.name.toUpperCase(),
-            regex           : args.name + '.*',
+            displayName               : args.name.toUpperCase(),
+            regex                     : args.name + '.*',
+            jenkinsfilePath           : isSandbox() ? 'Jenkinsfile_parameterized' : 'Jenkinsfile_CNP',
+            suppressDefaultJenkinsfile: false
     ] << args
     def name = config.name
 
-    String prodJenkinsfile = 'Jenkinsfile_CNP'
-    String sandboxJenkinsfile = 'Jenkinsfile_parameterized'
+    String jenkinsfilePath = config.jenkinsfilePath
+
+    String folderSandboxPrefix = isSandbox() ? 'Sandbox_' : ''
+    GString orgFolderName = "HMCTS_${folderSandboxPrefix}${name.toUpperCase()}"
+    wildcardBranchesToInclude = 'master masterv2 hmctsdemo demo cnp PR*'
+    GString orgDescription = "<br>${config.displayName} team repositories"
+
+    String displayNamePrefix = "HMCTS"
+
+    if (config.branchesToInclude) {
+        wildcardBranchesToInclude = config.branchesToInclude
+    }
+
+    boolean suppressDefaultJenkinsfile = config.suppressDefaultJenkinsfile
+
+    if (config.nightly) {
+        orgFolderName = "HMCTS_${folderSandboxPrefix}Nightly_${name.toUpperCase()}"
+        //noinspection GroovyAssignabilityCheck
+        orgDescription = "<br>Nightly tests for ${config.displayName}  will be scheduled using this organisation on the AAT Version of the application"
+
+        displayNamePrefix += " Nightly Tests"
+        wildcardBranchesToInclude = "master"
+
+        jenkinsfilePath = isSandbox() ? 'Jenkinsfile_nightly_sandbox' : 'Jenkinsfile_nightly'
+        suppressDefaultJenkinsfile = true
+    }
 
     return {
-        organizationFolder("HMCTS_${isSandbox() ? 'Sandbox_' : ''}${name.toUpperCase()}") {
-            description("${config.displayName} team repositories")
-            displayName("HMCTS - ${config.displayName}")
+        organizationFolder(orgFolderName) {
+            description(orgDescription)
+            displayName("${displayNamePrefix} - ${config.displayName}")
             organizations {
                 github {
-                    repoOwner("hmcts")
+                    repoOwner("HMCTS")
                     apiUri("https://api.github.com")
                     credentialsId("jenkins-github-hmcts-api-token_${name}")
                 }
@@ -44,10 +87,12 @@ Closure githubOrg(Map args = [:]) {
 
             projectFactories {
                 workflowMultiBranchProjectFactory {
-                    scriptPath(isSandbox() ? sandboxJenkinsfile : prodJenkinsfile)
+                    scriptPath(jenkinsfilePath)
                 }
-                workflowMultiBranchProjectFactory {
-                    scriptPath("Jenkinsfile")
+                if (!suppressDefaultJenkinsfile) {
+                    workflowMultiBranchProjectFactory {
+                        scriptPath("Jenkinsfile")
+                    }
                 }
             }
             configure { node ->
@@ -56,13 +101,20 @@ Closure githubOrg(Map args = [:]) {
                     regex(config.regex)
                 }
                 traits << 'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
-                    includes('master masterv2 hmctsdemo demo cnp PR*')
+                    includes(wildcardBranchesToInclude)
                 }
                 traits << 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
                     strategyId(1)
                 }
                 traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
                     strategyId(1)
+                }
+
+                // prevent builds triggering automatically from SCM push for sandbox and nightly builds
+                if (isSandbox() || config.nightly) {
+                    node / buildStrategies / 'jenkins.branch.buildstrategies.basic.NamedBranchBuildStrategyImpl'(plugin: 'basic-branch-build-strategies@1.1.1') {
+                        filters()
+                    }
                 }
             }
         }
