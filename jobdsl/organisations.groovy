35d169b5bff686c9ebc247991194e5c842e63193
Tim Jacomb <t.jacomb@kainos.com>
2019-02-01 10:49:51 +0000

    Add jobdsl support (#195)

---
+++ jobdsl/organisations.groovy
@@ -0,0 +1,146 @@
+String url = jenkins.model.JenkinsLocationConfiguration.get().getUrl()
+println "Running on ${url}"
+
+private boolean isSandbox() {
+    jenkins.model.JenkinsLocationConfiguration.get().getUrl().contains("sandbox")
+}
+
+List<Map> orgs = [
+        [name: 'CMC'],
+        [name: 'DIV', displayName: "Divorce"],
+        [name: 'CNP'],
+        [name: 'FinRem', displayName: 'Financial Remedy', regex: 'finrem.*'],
+        [name: 'CDM', regex: '\\b(?:document-management-store-app|dm-shared-infrastructure|ccd.*)\\b'],
+        [name: 'IAC', regex: 'ia.*'],
+        [name: 'Platform', regex: '(rpe-.*|send-letter.*|draft-store.*|bulk-scan.*|cmc-pdf-service|feature-toggle.*|private-beta-invitation.*|service-auth-provider-app|spring-boot-template)'],
+        [name: 'RPA'],
+        [name: 'SSCS'],
+        [name: 'Probate'],
+        [name: 'FeePay', displayName: 'Fees and Pay', regex: '(ccfr.*|ccpay.*|bar.*)'],
+        [name: 'SL', regex: 'snl.*'],
+        [name: 'DevOps', nightlyDisabled: true],
+        [name: 'IDAM', regex: '(idam-.*|cnp-idam-.*)'],
+        [name: 'CET'],
+        [name: 'FPL'],
+        [name: 'AM']
+]
+orgs.each { Map org ->
+    githubOrg(org).call()
+    org << [nightly: true]
+    if (!org.nightlyDisabled) {
+        githubOrg(org).call()
+    }
+}
+
+if (isSandbox()) {
+    Map pipelineTestOrg = [
+            name                      : 'Pipeline_Test',
+            displayName               : 'Pipeline Test',
+            regex                     : 'cnp-rhubarb-.*|cnp-jenkins-library',
+            branchesToInclude         : 'master',
+            jenkinsfilePath           : 'Jenkinsfile_pipeline_test',
+            suppressDefaultJenkinsfile: true
+    ]
+    githubOrg(pipelineTestOrg).call()
+}
+
+/**
+* Creates a github organisation
+* @param args map of arguments
+*  - name: the name of the organisation
+*  - displayName (optional, name will be used by default): display name, will be prefixed by HMCTS -
+*  - regex (optional, name.* will be used by default): regex to use for finding repos owned by this team
+*  - jenkinsfilePath (advanced use only): custom jenkinsfile path
+*  - suppressDefaultJenkinsfile: don't use the default Jenkinsfile
+*  - nightly: whether this is nightly org automatically set by the dsl
+*/
+Closure githubOrg(Map args = [:]) {
+    def config = [
+            displayName               : args.name,
+            regex                     : args.name.toLowerCase() + '.*',
+            jenkinsfilePath           : isSandbox() ? 'Jenkinsfile_parameterized' : 'Jenkinsfile_CNP',
+            suppressDefaultJenkinsfile: false
+    ] << args
+    def name = config.name
+
+    String jenkinsfilePath = config.jenkinsfilePath
+
+    String folderSandboxPrefix = isSandbox() ? 'Sandbox_' : ''
+    GString orgFolderName = "HMCTS_${folderSandboxPrefix}${name}"
+    wildcardBranchesToInclude = 'master masterv2 hmctsdemo demo cnp PR*'
+    GString orgDescription = "<br>${config.displayName} team repositories"
+
+    String displayNamePrefix = "HMCTS"
+
+    if (config.branchesToInclude) {
+        wildcardBranchesToInclude = config.branchesToInclude
+    }
+
+    boolean suppressDefaultJenkinsfile = config.suppressDefaultJenkinsfile
+
+    if (config.nightly) {
+        orgFolderName = "HMCTS_${folderSandboxPrefix}Nightly_${name}"
+        //noinspection GroovyAssignabilityCheck
+        orgDescription = "<br>Nightly tests for ${config.displayName}  will be scheduled using this organisation on the AAT Version of the application"
+
+        displayNamePrefix += " Nightly Tests"
+        wildcardBranchesToInclude = "master"
+
+        jenkinsfilePath = isSandbox() ? 'Jenkinsfile_nightly_sandbox' : 'Jenkinsfile_nightly'
+        suppressDefaultJenkinsfile = true
+    }
+
+    return {
+        organizationFolder(orgFolderName) {
+            description(orgDescription)
+            displayName("${displayNamePrefix} - ${config.displayName}")
+            organizations {
+                github {
+                    repoOwner("HMCTS")
+                    apiUri("https://api.github.com")
+                    credentialsId("jenkins-github-hmcts-api-token_${name.toLowerCase()}")
+                }
+            }
+
+            orphanedItemStrategy {
+                discardOldItems {
+                    daysToKeep(7)
+                    numToKeep(10)
+                }
+            }
+
+            projectFactories {
+                workflowMultiBranchProjectFactory {
+                    scriptPath(jenkinsfilePath)
+                }
+                if (!suppressDefaultJenkinsfile) {
+                    workflowMultiBranchProjectFactory {
+                        scriptPath("Jenkinsfile")
+                    }
+                }
+            }
+            configure { node ->
+                def traits = node / navigators / 'org.jenkinsci.plugins.github__branch__source.GitHubSCMNavigator' / traits
+                traits << 'jenkins.scm.impl.trait.RegexSCMSourceFilterTrait' {
+                    regex(config.regex)
+                }
+                traits << 'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
+                    includes(wildcardBranchesToInclude)
+                }
+                traits << 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
+                    strategyId(1)
+                }
+                traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
+                    strategyId(1)
+                }
+
+                // prevent builds triggering automatically from SCM push for sandbox and nightly builds
+                if (isSandbox() || config.nightly) {
+                    node / buildStrategies / 'jenkins.branch.buildstrategies.basic.NamedBranchBuildStrategyImpl'(plugin: 'basic-branch-build-strategies@1.1.1') {
+                        filters()
+                    }
+                }
+            }
+        }
+    }
+}
