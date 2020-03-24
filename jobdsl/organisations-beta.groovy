96fe94872646d1fe0cb5e9d390bd79f5c128e970
gbstan92 <35976278+gbstan92@users.noreply.github.com>
2020-03-24 17:48:56 +0000

    Moved probate to the new Beta Jenkins (#432)
    Co-authored-by: Bogdan Stanciu <bogdan.stanciu@hmcts.net>

--- jobdsl/organisations-beta.groovy
+++ jobdsl/organisations-beta.groovy
@@ -19,6 +19,7 @@ List<Map> orgs = [
         [name: 'MI', displayName: 'Management Information'],
         [name: 'PCQ'],
         [name: 'Platform',credentialId: "hmcts-jenkins-rpe", regex: '(rpe-.*|draft-store.*|cmc-pdf-service|feature-toggle.*|service-auth-provider-app|spring-boot-template|data-extractor|data-generator|camunda-.*)'],
+        [name: 'Probate'],
         [name: 'SSCS'],
         [name: 'XUI', regex: 'rpx-.*'],
 ]
