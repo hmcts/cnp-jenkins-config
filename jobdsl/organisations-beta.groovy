bf02429f686974a036ce292fff0f6cb686c253e8
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-04-21 15:11:59 +0100

    Swap IA to build-beta

--- jobdsl/organisations-beta.groovy
+++ jobdsl/organisations-beta.groovy
@@ -19,6 +19,7 @@ List<Map> orgs = [
         [name: 'ETHOS', displayName: "Ethos replacement"],
         [name: 'FinRem', displayName: "Financial Remedy"],
         [name: 'FPL'],
+        [name: 'IAC', regex: 'ia.*'],
         [name: 'MI', displayName: 'Management Information'],
         [name: 'PCQ'],
         [name: 'Platform',credentialId: "hmcts-jenkins-rpe", regex: '(rpe-.*|draft-store.*|cmc-pdf-service|feature-toggle.*|service-auth-provider-app|spring-boot-template|data-extractor|data-generator|camunda-.*)'],
