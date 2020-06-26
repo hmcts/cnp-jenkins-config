b0f15b42dac56621a0391a9b214ee34cfdc5296b
Tim Jacomb <tim.jacomb@hmcts.net>
2020-06-26 12:30:26 +0100

    Remove unused files

--- roles/jenkins/files/location.groovy
+++
@@ -1,6 +0,0 @@
-import jenkins.model.JenkinsLocationConfiguration
-
-def location = JenkinsLocationConfiguration.get()
-location.setUrl("${EXTERNAL_URL}")
-location.setAdminAddress("jenkins-reform@hmcts.net")
-location.save()
