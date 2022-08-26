827f58847e92df2406d9aeba079b5a587b284286
amstevenson <addstevenson@hotmail.com>
2022-08-26 14:14:28 +0100

    resolving indentation

--- jobdsl/organisations-beta.groovy
+++ jobdsl/organisations-beta.groovy
@@ -8,9 +8,9 @@ private boolean isSandbox() {
 }
 
 List<Map> orgs = [
-        [name: 'HMCTS_a_to_c', credentialsId: 'hmcts-jenkins-a-to-c', displayName: 'HMCTS - A to C', topic: 'jenkins-cft-a-c'],
-        [name: 'HMCTS_d_to_i', credentialsId: 'hmcts-jenkins-d-to-i', displayName: 'HMCTS - D to I', topic: 'jenkins-cft-d-i'],
-        [name: 'HMCTS_j_to_z', credentialsId: 'hmcts-jenkins-j-to-z', displayName: 'HMCTS - J to Z', topic: 'jenkins-cft-j-z']
+    [name: 'HMCTS_a_to_c', credentialsId: 'hmcts-jenkins-a-to-c', displayName: 'HMCTS - A to C', topic: 'jenkins-cft-a-c'],
+    [name: 'HMCTS_d_to_i', credentialsId: 'hmcts-jenkins-d-to-i', displayName: 'HMCTS - D to I', topic: 'jenkins-cft-d-i'],
+    [name: 'HMCTS_j_to_z', credentialsId: 'hmcts-jenkins-j-to-z', displayName: 'HMCTS - J to Z', topic: 'jenkins-cft-j-z']
 ]
 
 orgs.each { Map org ->
@@ -61,7 +61,7 @@ Closure githubOrg(Map args = [:]) {
     GString orgDescription = "<br>${config.displayName} team repositories"
 
     def orgDisplayName = config.displayName
-    
+
     String credId = config.credentialsId
 
     String folderSuffix = ''
