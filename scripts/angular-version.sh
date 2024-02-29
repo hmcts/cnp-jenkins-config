c82ed02357368b8944562979be4078bf3708f412
Rees Pozzi <reespozzi@icloud.com>
2024-02-29 10:21:49 +0000

    DTSPO-15800: Initial script and beginning of workflow

---
+++ scripts/angular-version.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+deprecation_config_file="deprecation-config.yml"
+# Use yq to extract the version for Angular Core
+angular_version=$(yq eval '.npm["angular/core"][0].version' "$deprecation_config_file")
+echo "Current version is: ${angular_version}"
+date_to_timestamp() {
+    date -jf "%Y-%m-%d" "$1" +%s
+}
+
+angular_eol_data=$(curl -s https://endoflife.date/api/angular.json | jq -c '.[]')
+
+# Get the current date in Unix timestamp
+current_date=$(date +%s)
+min_diff=""
+latest_supported_version=""
+
+for entry in ${angular_eol_data}; do
+    eol=$(echo "${entry}" | jq -r '.eol')
+    eol_date=$(date_to_timestamp "$eol")
+
+    # Calculate the difference between current date and end of life date
+    diff=$((eol_date - current_date))
+    abs_diff=${diff#-}
+
+    # Check if the difference is smaller than the minimum or if it's the first iteration
+    if [ -z "$min_diff" ] || [ "$abs_diff" -lt "$min_diff" ]; then
+        min_diff="$abs_diff"
+        latest_supported_version=$(echo "${entry}" | jq -r '.cycle')
+    fi
+done
+
+echo "Cycle with closest end of life date to current date: $latest_supported_version"
+
+if [[ $angular_version -lt $latest_supported_version ]];then
+    echo "New version ${latest_supported_version} needed in deprecation map"
+    yq eval -i '.npm["angular/core"][0].version = '\"$latest_supported_version\" $deprecation_config_file
+else
+    echo "File is showing most recent supported Angular version already"
+fi
+
