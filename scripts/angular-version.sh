15da32bf2bd44d292e3dc757acbd86c184f6ae62
Rees Pozzi <reespozzi@icloud.com>
2024-02-29 16:26:49 +0000

    Test againg

--- scripts/angular-version.sh
+++ scripts/angular-version.sh
@@ -10,7 +10,7 @@ date_to_timestamp() {
     date -d "$1" +%s
 }
 timestamp_to_date() {
-    date -d "@$1" "+%Y-%m-%d"
+    date -d "$1" "+%Y-%m-%d"
 }
 
 angular_eol_data=$(curl -s https://endoflife.date/api/angular.json | jq -c '.[]')
@@ -45,6 +45,7 @@ if [[ $angular_version -lt $latest_supported_version ]];then
     git checkout -b angular-update
     yq eval -i '.npm["angular/core"][0].version = '\"$latest_supported_version\" $deprecation_config_file
     # Add 30 days
+    echo $current_date
     one_month_from_now=$(expr $current_date + 2592000)
     one_month_from_now=$(timestamp_to_date "$one_month_from_now")
     yq eval -i '.npm["angular/core"][0].date_deadline = '\"$one_month_from_now\" $deprecation_config_file
